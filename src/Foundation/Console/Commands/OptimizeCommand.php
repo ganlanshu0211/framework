e0e9f2eef1a1db73668624430c589c8b76a1a4da
twilroad <269044570@qq.com>
2016-10-03 14:11:28 +0800

    添加优化命令

---
+++ src/Foundation/Console/Commands/OptimizeCommand.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Notadd.
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-09-30 13:58
+ */
+namespace Notadd\Foundation\Console\Commands;
+use Notadd\Foundation\Console\Abstracts\AbstractCommand;
+use Symfony\Component\Console\Input\InputOption;
+/**
+ * Class OptimizeCommand
+ * @package Notadd\Foundation\Console\Commands
+ */
+class OptimizeCommand extends AbstractCommand {
+    /**
+     * @return void
+     */
+    public function configure() {
+        $this->addOption('force', null, InputOption::VALUE_NONE, 'Force the compiled class file to be written.');
+        $this->addOption('psr', null, InputOption::VALUE_NONE, 'Do not optimize Composer dump-autoload.');
+        $this->setDescription('Optimize the framework for better performance');
+        $this->setName('optimize');
+    }
+    /**
+     * @return void
+     */
+    public function fire() {
+    }
+}
