6f0d7e12c4c979ccfd4b53548d55792bd5f06233
twilroad <269044570@qq.com>
2016-10-18 17:34:34 +0800

    重构Server实例化流程，并添加自定义版ServerRequest

--- src/Foundation/Http/Server.php
+++ src/Foundation/Http/Server.php
@@ -15,14 +15,34 @@ use Notadd\Foundation\Http\Pipelines\RouteDispatcher;
 use Notadd\Foundation\Http\Pipelines\SessionStarter;
 use Notadd\Install\InstallServiceProvider;
 use Notadd\Install\Pipelines\RedirectIfNotInstalled;
+use UnexpectedValueException;
+use Zend\Diactoros\Response;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Server as ZendServer;
+use Zend\Diactoros\ServerRequestFactory;
 use Zend\Stratigility\MiddlewarePipe;
 /**
  * Class Server
  * @package Notadd\Foundation\Http
  */
 class Server extends BaseServer {
+    /**
+     * @param callable $callback
+     * @param array $server
+     * @param array $query
+     * @param array $body
+     * @param array $cookies
+     * @param array $files
+     * @return \Zend\Diactoros\Server
+     */
+    public static function createServer(callable $callback, array $server, array $query, array $body, array $cookies, array $files) {
+        $server = ServerRequestFactory::normalizeServer($server ?: $_SERVER);
+        $files = ServerRequestFactory::normalizeFiles($files ?: $_FILES);
+        $headers = ServerRequestFactory::marshalHeaders($server);
+        $request = new ServerRequest($server, $files, ServerRequestFactory::marshalUriFromServer($server, $headers), ServerRequestFactory::get('REQUEST_METHOD', $server, 'GET'), 'php://input', $headers, $cookies ?: $_COOKIE, $query ?: $_GET, $body ?: $_POST, static::marshalProtocolVersion($server));
+        $response = new Response();
+        return new ZendServer($callback, $request, $response);
+    }
     /**
      * @param \Notadd\Foundation\Application $app
      * @return \Zend\Stratigility\MiddlewareInterface
@@ -44,7 +64,7 @@ class Server extends BaseServer {
             $pipe->pipe($path, new ErrorHandler($errorDir, true, $app->make('log')));
         } else {
             $pipe->pipe($path, function () use ($errorDir) {
-                return new HtmlResponse(file_get_contents($errorDir.'/503.html', 503));
+                return new HtmlResponse(file_get_contents($errorDir . '/503.html', 503));
             });
         }
         return $pipe;
@@ -78,7 +98,20 @@ class Server extends BaseServer {
      */
     public function listen() {
         $app = $this->getApp();
-        $server = ZendServer::createServer($this->getMiddleware($app), $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
+        $server = static::createServer($this->getMiddleware($app), $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
         $server->listen();
+    }
+    /**
+     * @param array $server
+     * @return string
+     */
+    protected static function marshalProtocolVersion(array $server) {
+        if(!isset($server['SERVER_PROTOCOL'])) {
+            return '1.1';
+        }
+        if(!preg_match('#^(HTTP/)?(?P<version>[1-9]\d*(?:\.\d)?)$#', $server['SERVER_PROTOCOL'], $matches)) {
+            throw new UnexpectedValueException(sprintf('Unrecognized protocol version (%s)', $server['SERVER_PROTOCOL']));
+        }
+        return $matches['version'];
     }
 }
