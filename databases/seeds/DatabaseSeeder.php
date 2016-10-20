db88416ac866b0b76c61465f45fbf65f5331167f
twilroad <269044570@qq.com>
2016-10-20 11:24:34 +0800

    添加默认seeder类

---
+++ databases/seeds/DatabaseSeeder.php
@@ -0,0 +1,14 @@
+<?php
+use Notadd\Foundation\Database\Seeder;
+/**
+ * Class DatabaseSeeder
+ */
+class DatabaseSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run() {
+        $this->command->getOutput()->writeln('<info>There is Nothing to seed!</info>');
+    }
+}
